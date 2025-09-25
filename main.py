5706229cbdb492c50316013b072a274ab3afd8f8
bgubka251-crypto <bgubka251@gmail.com>
2025-09-25 11:50:43 +0300

    Этап 1 с исправлениями

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #Вариант 28
 
 import os
+import getpass
+import socket
 
 def expand(s:str):
     for k in os.environ:
         s = s.replace("$" + k, os.environ[k])
     return s
 
+username = getpass.getuser()
+hostname = socket.gethostname()
+
+
 while True:
-    raw = expand(input("username@hostname:~ $ "))
+    raw = expand(input(username + "@" + hostname + ":~ $ "))
     cmd, *args = raw.split()
     if cmd == "exit":
         break
